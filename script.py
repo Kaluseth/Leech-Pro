c572d7cc128c93ac5be6114a41354166a2f61b07
Kaluseth <101351762+Kaluseth@users.noreply.github.com>
2022-03-13 21:28:15 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,65 @@
+class script(object):
+
+    START_TEXT = """<b>Hai ,
+    
+I'm A disney team Zee5 link downloader bot With Permanent Thumbnail Support馃挴.
+
+Please send me any Zee5 link, I can upload it to telegram as File/Video without using your data.
+
+Currently I'm in beta mode 馃ズ, If found any bugs, report @disneyteamchat !
+
+Click <i>/help</i> for more details....</b>"""
+
+
+    HELP_USER = """<b>Hai, Follow these steps..</b>
+ 
+1. Send Custom Thumbnail (It will be saved permenantly!)
+
+2. Send your zee5 url and select desired option.
+
+
+NOTE: Download may take some time! So please wait for it to complete!"""
+
+
+    ABOUT_TEXT = """猸曪笍<b>My Name : Disney Team Zee5 DL</b>
+
+猸曪笍<b>Creater :</b> @doreamonfans1
+
+猸曪笍<b>Language :</b> <code>Python3</code>
+
+猸曪笍<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+
+猸曪笍<b>updates channel :</b> 馃憠 <a href='https://t.me/disneygrou'>Disney Team</a>"""
+
+
+
+    FORMAT_SELECTION = """<b>Choose appropriate option</b> <a href='{}'>猬囷笍</a>
+
+馃帪  - Stream format
+馃搧  - File format
+
+<i>NOTE : Taking high resolutions may result in files above 2GB and hence cannot Upload to TG. So better select a medium resolution.</i> 馃槆
+"""    
+    
+    UPGRADE_TEXT = "PING at @disneyteamzee5dlbot"
+    
+    DOWNLOAD_START = "Trying to download to my server. This may take a while 馃槾"
+    
+    UPLOAD_START = "Uploading Now 猬嗭笍"
+    
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
+
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "**Thank you for Using doreamon fans bot!! 鉂わ笍**"
+    
+    SAVED_CUSTOM_THUMB_NAIL = "<b>鉁匔ustom thumbnail Saved.\nThis thumbnail will be Permanent for all future uploads\n\nDo /delthumb to clear your thumbnail!</b>"
+    
+    DEL_ETED_CUSTOM_THUMB_NAIL = "鉁� Custom Thumbnail cleared succesfully."
+    
+    SHOW_THUMB = "@disneyteamzee5dlbot\n\nUse /delthumb to clear this thumbnail."
+    
+    NO_THUMB = "SED馃槙 No saved thumbnails Found!!"
+    
+    CUSTOM_CAPTION_UL_FILE = "<b>{newname}\n\n漏锔� @doreamonfans2</b>"
+    
+    TIMEOUT = "<b><i>Sorry for the delay. It'll help reduce the flood wait</i> 馃槆\n\nWait for {} sec and try again.</b>"
+    
